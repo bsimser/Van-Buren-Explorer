2aedc16ff85646ebceaba7ccac1d89b4133405cc
Bil Simser <bsimser@shaw.ca>
2021-09-02 10:09:04 -0600

    Added tga file specification

---
+++ VanBurenExplorerLib/Specs/TgaFileSpecification.cs
@@ -0,0 +1,12 @@
+﻿using VanBurenExplorerLib.Models;
+
+namespace VanBurenExplorerLib.Specs
+{
+    public class TgaFileSpecification : IFileSpecification
+    {
+        public bool IsSatisfiedBy(FileProperties properties)
+        {
+            return properties.FullPath.EndsWith(".tga");
+        }
+    }
+}
